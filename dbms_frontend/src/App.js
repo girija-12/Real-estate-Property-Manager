5aa44ea803860cd81559a9e06794ac907ec19400
Girija <ramagirija95@gmail.com>
2025-03-29 17:51:24 +0530

    first login page

--- dbms_frontend/src/App.js
+++ dbms_frontend/src/App.js
@@ -15,6 +15,7 @@ function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         
